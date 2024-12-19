90c56d25ad4382a52fc132ff9375cdb57a45084f
serverhorror <serverhorror@users.noreply.github.com>
2024-12-19 16:36:23 +0000

    chore: note default keybinding methods / remapping

--- dot-config/nvim/lua/serverhorror/plugins/init.lua
+++ dot-config/nvim/lua/serverhorror/plugins/init.lua
@@ -82,6 +82,15 @@ return {
 			-- make sure ~/src is in the workspace root and
 			-- set the workspace folder to the root of the project
 			vim.g.copilot_workspace_folders = { vim.fn.getcwd(), "~/src" }
+			-- -- Disable default key mappings
+			-- vim.g.copilot_no_tab_map = true
+			-- vim.g.copilot_assume_mapped = true
+			-- vim.g.copilot_tab_fallback = ""
+
+			-- -- Set keybindings for Copilot
+			-- vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+			-- vim.api.nvim_set_keymap("i", "<C-n>", "copilot#Next()", { silent = true, expr = true })
+			-- vim.api.nvim_set_keymap("i", "<C-p>", "copilot#Previous()", { silent = true, expr = true })
 		end,
 	},
 	-- END GitHub Copilot
