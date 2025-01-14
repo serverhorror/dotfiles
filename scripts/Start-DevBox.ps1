125b65379bab3f6cd0dc5748fd1c2380cdeed4cd
serverhorror <serverhorror@users.noreply.github.com>
2025-01-14 09:45:22 +0000

    2025-01-14T10:45:22+0100

--- scripts/Start-DevBox.ps1
+++ scripts/Start-DevBox.ps1
@@ -62,6 +62,74 @@ function Start-DevBox {
             throw "Could not find a valid session for profile $AWSProfile."
         }
         Write-Debug "$(Get-TimeStamp) - AWS Session: $awsSession"
+
+
+
+        # & aws sso --profile "$AWSProfile" login
+        $provisionedProducts = $(& aws --profile $AWSProfile servicecatalog list-provisioned-product-plans | ConvertFrom-Json)
+        Write-Debug "$(Get-TimeStamp) - Provisioned Products: $provisionedProducts"
+
+        # check if provisionedProducts has a key ProvisionedProductPlans and if it has at least one element
+        if ($null -eq $provisionedProducts.ProvisionedProductPlans) {
+            Write-Error "$(Get-TimeStamp) - No provisioned product plans found."
+            return
+        }
+
+        if ($null -ne $provisionedProducts.ProvisionedProductPlans -and $provisionedProducts.ProvisionedProductPlans.Count -gt 0) {
+            $provisionedProductId = $provisionedProducts.ProvisionedProductPlans[0].ProvisionProductId
+            Write-Debug "$(Get-TimeStamp) - Provisioned Product Id: $provisionedProductId"
+
+            $productDetails = $(& aws --profile $AWSProfile servicecatalog describe-provisioned-product --id $provisionedProductId | ConvertFrom-Json)
+            $productId = $productDetails.ProvisionedProductDetail.ProductId
+            Write-Debug "$(Get-TimeStamp) - Product Id: $productId"
+        }
+        else {
+            Write-Error "$(Get-TimeStamp) - No provisioned product plans found."
+            return
+        }
+        $provisioningArtifactId = $provisionedProducts.ProvisionedProductPlans[0].ProvisioningArtifactId
+        Write-Debug "$(Get-TimeStamp) - Provisioning Artifact Id: $provisioningArtifactId"
+
+        if (-Not $DevBox) {
+            # get the provisioned product outputs
+            Write-Debug "$(Get-TimeStamp) - Getting the provisioned product outputs."
+            $provisionedProductOutputs = aws servicecatalog get-provisioned-product-outputs --profile $AWSProfile --provisioned-product-id $provisionedProductId
+            $DevBox = ($($provisionedProductOutputs | ConvertFrom-Json).Outputs | Where-Object { $_.OutputKey -eq 'Instance' }).OutputValue
+            Write-Debug "$(Get-TimeStamp) - Found DevBox: $DevBox"
+        }
+
+        $serviceCatalogActions = $(& aws --profile $AWSProfile servicecatalog list-service-actions-for-provisioning-artifact --product-id "$productId" --provisioning-artifact-id "$provisioningArtifactId" | ConvertFrom-Json)
+        Write-Debug "$(Get-TimeStamp) - Service Catalog Action Id: $serviceCatalogActions"
+
+        # get the service action id that starts the devbox, the name of the action is "Start-EC2"
+        $serviceActionStartEc2Id = $serviceCatalogActions.ServiceActionSummaries | Where-Object { $_.Name -eq "Start-EC2" } | Select-Object -ExpandProperty Id
+        Write-Debug "$(Get-TimeStamp) - Service Action Start EC2 Id: $serviceActionStartEc2Id"
+
+        $executionResult = & aws --profile "$AWSProfile" servicecatalog execute-provisioned-product-service-action --provisioned-product-id $provisionedProductId --service-action-id $serviceActionStartEc2Id
+        Write-Debug "$(Get-TimeStamp) - Execution Result: $executionResult"
+
+
+        # busy polling of the status of the provisioned product for at most 30 seconds
+        $waitPeriod = 300
+        $timeout = [datetime]::Now.AddSeconds($waitPeriod)
+        $desiredStatus = "AVAILABLE"
+        Write-Output "$(Get-TimeStamp) - Triggering the start of the $DevBox ... "
+        Write-Output "Starting $DevBox ... (wait for $waitPeriod seconds)"
+        while ($status -ne "$desiredStatus" -or [datetime]::Now -ge $timeout) {
+            Start-Sleep -Seconds 3
+            $status = $(& aws --profile $AWSProfile servicecatalog describe-provisioned-product --id $provisionedProductId | ConvertFrom-Json).ProvisionedProductDetail.Status
+            Write-Progress -Activity "Starting DevBox: $DevBox" -Status "Status: $status" -SecondsRemaining ($timeout - [datetime]::Now).TotalSeconds
+        }
+        write-progress -Activity "Starting DevBox: $DevBox" -completed
+
+        if ($status -ne "$desiredStatus") {
+            Write-Error "$(Get-TimeStamp) -  xxx I don't know what to do! Send help! xxx"
+            Write-Warning "$(Get-TimeStamp) - Timeout reached. The status is: $status."
+            exit 1
+        }
+        else {
+            Write-Debug "$(Get-TimeStamp) - The status has changed to $status."
+        }
     }
     catch {
         # Login to AWS SSO
@@ -71,74 +139,6 @@ function Start-DevBox {
             throw "Could not find a valid profile $AWSProfile."
         }
     }
-
-
-    # & aws sso --profile "$AWSProfile" login
-    $provisionedProducts = $(& aws --profile $AWSProfile servicecatalog list-provisioned-product-plans | ConvertFrom-Json)
-    Write-Debug "$(Get-TimeStamp) - Provisioned Products: $provisionedProducts"
-
-    # check if provisionedProducts has a key ProvisionedProductPlans and if it has at least one element
-    if ($null -eq $provisionedProducts.ProvisionedProductPlans) {
-        Write-Error "$(Get-TimeStamp) - No provisioned product plans found."
-        return
-    }
-
-    if ($null -ne $provisionedProducts.ProvisionedProductPlans -and $provisionedProducts.ProvisionedProductPlans.Count -gt 0) {
-        $provisionedProductId = $provisionedProducts.ProvisionedProductPlans[0].ProvisionProductId
-        Write-Debug "$(Get-TimeStamp) - Provisioned Product Id: $provisionedProductId"
-
-        $productDetails = $(& aws --profile $AWSProfile servicecatalog describe-provisioned-product --id $provisionedProductId | ConvertFrom-Json)
-        $productId = $productDetails.ProvisionedProductDetail.ProductId
-        Write-Debug "$(Get-TimeStamp) - Product Id: $productId"
-    }
-    else {
-        Write-Error "$(Get-TimeStamp) - No provisioned product plans found."
-        return
-    }
-    $provisioningArtifactId = $provisionedProducts.ProvisionedProductPlans[0].ProvisioningArtifactId
-    Write-Debug "$(Get-TimeStamp) - Provisioning Artifact Id: $provisioningArtifactId"
-
-    if (-Not $DevBox) {
-        # get the provisioned product outputs
-        Write-Debug "$(Get-TimeStamp) - Getting the provisioned product outputs."
-        $provisionedProductOutputs = aws servicecatalog get-provisioned-product-outputs --profile $AWSProfile --provisioned-product-id $provisionedProductId
-        $DevBox = ($($provisionedProductOutputs | ConvertFrom-Json).Outputs | Where-Object { $_.OutputKey -eq 'Instance' }).OutputValue
-        Write-Debug "$(Get-TimeStamp) - Found DevBox: $DevBox"
-    }
-
-    $serviceCatalogActions = $(& aws --profile $AWSProfile servicecatalog list-service-actions-for-provisioning-artifact --product-id "$productId" --provisioning-artifact-id "$provisioningArtifactId" | ConvertFrom-Json)
-    Write-Debug "$(Get-TimeStamp) - Service Catalog Action Id: $serviceCatalogActions"
-
-    # get the service action id that starts the devbox, the name of the action is "Start-EC2"
-    $serviceActionStartEc2Id = $serviceCatalogActions.ServiceActionSummaries | Where-Object { $_.Name -eq "Start-EC2" } | Select-Object -ExpandProperty Id
-    Write-Debug "$(Get-TimeStamp) - Service Action Start EC2 Id: $serviceActionStartEc2Id"
-
-    $executionResult = & aws --profile "$AWSProfile" servicecatalog execute-provisioned-product-service-action --provisioned-product-id $provisionedProductId --service-action-id $serviceActionStartEc2Id
-    Write-Debug "$(Get-TimeStamp) - Execution Result: $executionResult"
-
-
-    # busy polling of the status of the provisioned product for at most 30 seconds
-    $waitPeriod = 300
-    $timeout = [datetime]::Now.AddSeconds($waitPeriod)
-    $desiredStatus = "AVAILABLE"
-    Write-Output "$(Get-TimeStamp) - Triggering the start of the $DevBox ... "
-    Write-Output "Starting $DevBox ... (wait for $waitPeriod seconds)"
-    while ($status -ne "$desiredStatus" -or [datetime]::Now -ge $timeout) {
-        Start-Sleep -Seconds 3
-        $status = $(& aws --profile $AWSProfile servicecatalog describe-provisioned-product --id $provisionedProductId | ConvertFrom-Json).ProvisionedProductDetail.Status
-        Write-Progress -Activity "Starting DevBox: $DevBox" -Status "Status: $status" -SecondsRemaining ($timeout - [datetime]::Now).TotalSeconds
-    }
-    write-progress -Activity "Starting DevBox: $DevBox" -completed
-
-    if ($status -ne "$desiredStatus") {
-        Write-Error "$(Get-TimeStamp) -  xxx I don't know what to do! Send help! xxx"
-        Write-Warning "$(Get-TimeStamp) - Timeout reached. The status is: $status."
-        exit 1
-    }
-    else {
-        Write-Debug "$(Get-TimeStamp) - The status has changed to $status."
-    }
-
     try {
         # start a code instance in the devbox via remote tunnels
         & ssh -T $DevBox "echo 'Good day sir!'"
