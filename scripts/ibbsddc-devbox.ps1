c5a83bf47a7379da2ae41bb3496e050eabf2294c
serverhorror <serverhorror@users.noreply.github.com>
2025-01-31 18:53:21 +0100

    feat: update SSH, AWS configurations and add new SSH scripts for devboxes

---
+++ scripts/ibbsddc-devbox.ps1
@@ -0,0 +1,8 @@
+# Enable ANSI escape sequences
+$esc = [char]27
+
+# Set AWS profile
+$awsProfile = "bi-is-ideationspace-ibbsddc-teamspace"
+Write-Host -ForegroundColor Red "Using AWS profile: $esc[1m$awsProfile$esc[0m"
+aws --profile "$awsProfile" sso login
+ssh ibbsddc-devbox
---
+++ scripts/play-devbox.ps1
@@ -0,0 +1,8 @@
+# Enable ANSI escape sequences
+$esc = [char]27
+
+# Set AWS profile
+$awsProfile = "bi-is-ideationspace-play-teamspace"
+Write-Host -ForegroundColor Red "Using AWS profile: $esc[1m$awsProfile$esc[0m"
+aws --profile "$awsProfile" sso login
+ssh play-devbox
