9d689b3eefb7ef795693fd1a998e34b469e3b822
serverhorror <serverhorror@users.noreply.github.com>
2025-02-23 01:58:00 +0100

    feat/emacs: intial commit

---
+++ dot-config/emacs/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(leuven-dark))
+ '(custom-safe-themes
+   '("937401a2e532f2c8c881b6b3f20d9d4b6b9405bccf72ea6289c9d3f4507eb1ab" default))
+ '(display-line-numbers-type t)
+ '(line-move-visual t)
+ '(package-selected-packages '(magit modus-themes))
+ '(require-final-newline t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
