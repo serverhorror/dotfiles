6b0d20942a9c64f5de2927175392236ad4ab72f8
serverhorror <serverhorror@users.noreply.github.com>
2025-05-10 16:30:00 +0200

    feat/nvim: disable line wrapping by default

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -12,6 +12,8 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- disable wrapping
+vim.opt.wrap = false
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
