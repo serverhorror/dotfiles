638cdb5eefdf3825a5b33c2ebe61d55d09dfbea8
serverhorror <serverhorror@users.noreply.github.com>
2025-05-13 15:15:21 +0200

    feat: add scripts for Discord and Slack launchers, update Hyprland config, and Waybar settings

---
+++ dot-config/hypr/scripts/workspace-names.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+hyprctl dispatch renameworkspace 1 "Main"
+hyprctl dispatch renameworkspace 2 "Work"
+hyprctl dispatch renameworkspace 3 "Code"
+hyprctl dispatch renameworkspace 4 "Gimp"
+hyprctl dispatch renameworkspace 10 "Utils"
